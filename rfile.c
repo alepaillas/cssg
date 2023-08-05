cb8ecebe3c6a3c2dcfb2e80552b8f5c7efccb477
mulmeyun <mulmeyun@godspeed.moe>
2023-08-05 15:59:50 -0400

    added command line options

--- rfile.c
+++ rfile.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
-void readfile (FILE* ptr, char ch)
+static int verbose_flag;
+
+void readfile (FILE* ptr)
 {
+	char ch;
+
 	ptr = fopen("test.txt", "r");
 
 	if (NULL == ptr) {
@@ -21,12 +26,51 @@ void readfile (FILE* ptr, char ch)
 	fclose(ptr);
 }
 
-int main()
-{
-	FILE* ptr;
-	char ch;
+int main(int argc, char **argv) {
+	// for parsing command line arguments
+	static struct option long_options[] = {
+		{ "verbose", 0, NULL, 'v' },
+		{ "help", 0, NULL, 'h' },
+		{ "file", 1, NULL, 'f' },
+		{ 0, 0, 0, 0 }
+	};
 
-	readfile (ptr, ch);
+	int option, index;
+
+	opterr = 0;
+
+	while ((option = getopt_long(argc, argv, "vhf:", long_options, NULL)) != -1){
+		switch (option) {
+			case 'v':
+				verbose_flag = 1;
+				printf("verbose_flag is %d\n", verbose_flag);
+				break;
+			case 'h':
+				printf("usage: %s [-h] [-f file.html]\n", argv[0]);
+				break;
+			case 'f':
+				printf("file is '%s'\n", optarg);
+				break;
+			case '?':
+				fprintf(stderr, "Bad syntax. See %s -h\n", argv[0]);
+				return 1;
+			default:
+				abort();
+		}
+	}
+
+	/* Print any remaining command line arguments (not options). */
+	if (optind < argc){
+		printf ("non-option ARGV-elements: ");
+		while (optind < argc)
+			printf ("%s ", argv[optind++]);
+		putchar ('\n');
+		return 1;
+	}
+
+	FILE* ptr;
+
+	readfile (ptr);
 	
 	return 0;
 }
